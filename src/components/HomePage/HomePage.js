a335d7fa870f99666b6920be6d8a9e6474b09564
Sahil Naikdhure <sanaikdhure@gmail.com>
2021-02-09 19:42:36 +0530

    add text and sub-text

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -2,7 +2,14 @@ import React from 'react';
 import './HomePage.css'
 
 const HomePage = () => {
-    return (<div className="home-page" >This is mtvLib</div>)
+    return (
+    <div className="home-page" >
+        <div className="text-container" >
+            <h1>MTVLIB</h1>
+            <p>Movies. TV. Library.</p>
+            <p>Decide what you want to binge on next, here.</p>
+        </div>
+    </div>)
 }
 
 export default HomePage;
