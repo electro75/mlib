408f184a418d80746daecff02bd25a1ef057bc53
Sahil Naikdhure <sanaikdhure@gmail.com>
2021-02-11 14:33:33 +0530

    add banners on homepage

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -1,14 +1,35 @@
 import React from 'react';
-import './HomePage.css'
+import './HomePage.css';
 
-const HomePage = () => {
+
+const HomePage = () => {    
+
     return (
-    <div className="home-page" >
-        <div className="text-container" >
-            <h1 className="title-name" >MTVLIB</h1>
-            <p>Find your next binge here.</p>
+        <div className="home-page" >
+            <div className="home-page-banner" >
+                <div className="text-container" >
+                    <h1 className="title-name" >MTVLIB</h1>
+                    <p>Find your next binge here.</p>
+                </div>
+            </div>
+            <div className="explore-banner m-banner" >
+                <div className="banner-cover" >
+                    <div className="explore-btn" >
+                        Explore Movies
+                    </div>
+                </div>
+                
+            </div>
+            <div className="explore-banner t-banner" >
+                <div className="banner-cover" >
+                    <div className="explore-btn" >
+                        Explore TV
+                    </div>
+                </div>
+                
+            </div>
         </div>
-    </div>)
+    )
 }
 
 export default HomePage;
